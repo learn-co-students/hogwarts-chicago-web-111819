e31f9de6120c5d4c3b0c320f718d389f3c68086d
lluisguardiola <lluisguardiola.h@gmail.com>
2020-01-30 12:01:26 -0600

    passing single hog object to HogCard

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-export default class CardBoi extends React.Component {
+
+export default class HogCard extends React.Component {
+    
 
     render () {
         return(
