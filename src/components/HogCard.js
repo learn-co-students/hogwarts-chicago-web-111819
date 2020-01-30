ba58d37e0c0fcadd8170afcb2d5cc1affdcf5f9d
github username <github email address>
2020-01-30 12:06:04 -0600

    HogCard is setup with good struct. Heart button is functional

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
-export default class CardBoi extends React.Component {
+export default class HogCard extends React.Component {
+
+    heartBoi = (e) => {
+        if (e.target.innerText === '♡'){
+            e.target.innerText = '♥'
+        } else if (e.target.innerText === '♥'){
+            e.target.innerText = '♡'
+        }
+    }
 
     render () {
         return(
             <div className="ui card">
-                <div className="content">
-                    <div className="right floated meta">14h</div>
-                    <img className="ui avatar image" src="/images/avatar/large/elliot.jpg" /> Elliot
+                <div className="image">
+                    <img src={require("../hog-imgs/cherub.jpg")} />
+                </div>
+                <div>
+                    <h1>Cherub</h1>
                 </div>
                 <div className="image">
-                    <img />
                 </div>
                 <div className="content">
-                    <span className="right floated">
-                    <i className="heart outline like icon"></i>
-                    17 likes
-                    </span>
-                    <i className="comment icon"></i>
-                    3 comments
+                    <p>Specialty:{}</p>
+                    <button>More Info</button><button>Hide Me</button>
                 </div>
                 <div className="extra content">
-                    <div className="ui large transparent left icon input">
-                    <i className="heart outline icon"></i>
-                    <input type="text" placeholder="Add Comment..." />
-                    </div>
+                    <p onClick={this.heartBoi}>♡</p>
                 </div>
             </div>
         )
     }
 }
+// ♥
