ddf13a038e556721aa62e9c412e26f2b7e2f5753
dut-a <d.dut@live.com>
2020-01-31 02:20:00 -0600

    Partially completes sorting

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,58 @@ class App extends Component {
     super();
     this.state = {
       hogs: hogs,
-      moreInfo: false
+      moreInfo: false,
+      greasedHogs: false,
+      sortOrder: "none"
     };
   }
+
+  toggleGreasedHogs = () => {
+    this.setState({
+      greasedHogs: !this.state.greasedHogs
+    });
+  }
+
+  toggleSortOrder = e => {
+    let sortOption = e.target.value;
+    this.setState({
+      sortOrder: sortOption
+    });
+  }
+
+  filterGreasedHogs = () => {
+    const currentHogs = this.state.hogs;
+    let filteredHogs = this.state.greasedHogs ? 
+    currentHogs.filter(hog => hog.greased) : currentHogs;
+    return filteredHogs;
+  }
+
+  sortHogs = currentHogs => {
+    let sortedHogs = currentHogs;
+    switch (this.state.sortOrder) {
+      case "name":
+        sortedHogs = currentHogs.sort((hog1, hog2) => hog1.name.localeCompare(hog2.name));
+        break;
+
+      case "weight":
+        sortedHogs = currentHogs.sort((hog1, hog2) => hog1.weight - hog2.weight);
+        break;
+
+      default:
+        break;
+    }
+    return sortedHogs;
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
-        <HogsContainer currentHogs={this.state.hogs}/>
+        <HogsContainer
+          filterHogs={this.toggleGreasedHogs}
+          sortOrder={this.toggleSortOrder}
+          currentHogs={this.sortHogs(this.filterGreasedHogs()) }
+        />
       </div>
     );
   }
--- src/components/HogsContainer.js
+++ src/components/HogsContainer.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import HogTile from './HogTile';
 
 export default class HogsContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      someCrap: null
-    }
-  }
+
   render() {
+    let sortOpts = ["Name", "Weight"];
     return (
       <div>
       <div>
-        <span><input type="checkbox" /> Show only greased hogs</span>
+        <span><input type="checkbox" onChange={this.props.filterHogs} /> Show only greased hogs</span>
+        &nbsp;|&nbsp;
+        <select onChange={this.props.sortOrder}>
+          <option value="---select---" disabled="disabled" defaultChecked={true}>---select---</option>
+          <option value={sortOpts[0].toLowerCase()}>{sortOpts[0]}</option>
+          <option value={sortOpts[1].toLowerCase()}>{sortOpts[1]}</option>
+        </select>
         <hr />
       </div>
       <div className="ui grid container cards">
-        {
-        this.props.currentHogs.map((hogData, i) => {
-          return <HogTile hog={hogData} key={i} />
-        })}
+        { this.props.currentHogs.map((hogData, i) => <HogTile hog={hogData} key={i} />) }
       </div>
       </div>
     );
